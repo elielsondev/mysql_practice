8cc0b849e5138336a3da3dc1e484af53dfb608db
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-05-14 20:51:58 -0300

    feat - COUNT - contando resultados

---
+++ 06_COUNT.sql
@@ -0,0 +1,9 @@
+-- COUNT - contando resultados:
+SELECT COUNT(*) FROM escola.estudantes;
+SELECT COUNT(nome) FROM escola.estudantes;
+SELECT COUNT(DISTINCT nome) FROM escola.estudantes;
+SELECT COUNT(DISTINCT nome, idade) FROM escola.estudantes;
+SELECT COUNT(nome) FROM escola.estudantes WHERE nome = 'Maria';
+
+-- Atenção: O comando COUNT não retorna dados nulos (NULL em SQL), entretanto valores vazios
+-- (como uma string vazia, por exemplo: "") são considerados caracteres e por isso serão contados.
