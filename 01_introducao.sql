5244aac0558378bd6f884dd7b5a06895a2c2549c
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-05-14 20:08:47 -0300

    feat - Introdução

---
+++ 01_introducao.sql
@@ -0,0 +1,26 @@
+-- Exibe todos os bancos de dados:
+SHOW DATABASES;
+
+-- Referência o banco de dados que será utilizado na query:
+USE sakila;
+
+-- Retorna todas as tabelas inicializadas no seu server:
+SHOW TABLES;
+
+-- Visualizar a estrutura de uma tabela:
+DESCRIBE actor;
+
+-- Criar um banco de dados:
+CREATE DATABASE nome_do_novo_banco_de_dados;
+
+-- Constraints (restrições), chaves primárias e chaves estrangeiras:
+	-- NOT NULL
+    -- UNIQUE
+    -- PRIMARY KEY
+    -- FOREIGN KEY
+    -- DEFAULT 
+    
+-- Como os dados são ligados?
+	-- 1:1 - Um para um;
+	-- 1:N - Um para muitos;
+	-- N:N - Muitos para muitos;
