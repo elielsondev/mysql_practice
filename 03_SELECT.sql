6885873e957e673df0f5c29f85142c3ab3851658
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-05-14 20:20:04 -0300

    feat - SELECT

---
+++ 03_SELECT.sql
@@ -0,0 +1,10 @@
+-- SELECT:
+SELECT 'Hello, World!';
+SELECT 'Hello, World!' AS me_livrei_da_maldição;
+SELECT NOW();
+SELECT 20 * 10;
+SELECT 20 / 10;
+SELECT 20 + 10;
+SELECT 20 - 10;
+SELECT 'Elielson', 'Nascimento', 32, 'Desenvolvimento Web Full-Stack Jr.';
+SELECT 'Elielson' AS nome, 'Nascimento' AS sobrenome, 32 AS idade, 'Desenvolvimento Web Full-Stack Jr.' AS profissão;
