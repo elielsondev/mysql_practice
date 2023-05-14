c90d6b04a9c5fd391029fe6d2b4e0e878ac0af5f
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-05-14 20:18:17 -0300

    feat - sintaxe para acessar uma tabela

---
+++ 02_sintaxe_para_acessar_tabela.sql
@@ -0,0 +1,12 @@
+-- Sintaxe para acessar uma tabela:
+
+-- SELECT nome_da_coluna FROM nome_do_banco.nome_da_tabela;
+	-- ou
+-- USE nome_do_banco;
+-- SELECT nome_da_coluna FROM nome_da_tabela;
+
+-- Exemplo:
+SELECT first_name FROM sakila.actor;
+-- ou
+USE sakila;
+SELECT first_name FROM actor;
