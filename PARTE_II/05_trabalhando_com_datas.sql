a680312bc44749bbd7dab73cb25da477f87fb54d
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-05-26 23:03:24 -0300

    feat - Trabalhando com datas

---
+++ PARTE_II/05_trabalhando_com_datas.sql
@@ -0,0 +1,43 @@
+-- Trabalhando com datas:
+
+/*
+Conseguimos fazer algumas coisas legais, como, por exemplo, 
+consultar data e hora atuais usando as seguintes funções:
+*/
+SELECT CURRENT_DATE(); -- YYYY-MM-DD
+SELECT NOW(); -- YYYY-MM-DD HH:MM:SS
+
+
+/*
+Também podemos calcular a diferença em dias entre duas datas usando o DATEDIFF
+e a diferença de tempo entre dois horários usando o TIMEDIFF. Em ambos os casos,
+o segundo valor é subtraído do primeiro para calcular o resultado.
+*/
+
+SELECT DATEDIFF('2020-01-31', '2020-01-01');
+-- 30, ou seja, a primeira data é 30 dias depois da segunda
+
+SELECT DATEDIFF('2020-01-01', '2020-01-31');
+-- -30, ou seja, a primeira data é 30 dias antes da segunda
+
+SELECT TIMEDIFF('08:30:10', '09:30:10');
+-- -01:00:00, ou seja, há 1 hora de diferença entre os horários
+
+SELECT TIMEDIFF('2021-08-11 08:30:10', '2021-08-01 09:30:10');
+-- -239:00:00, ou seja, há uma diferença de 239 horas entre as datas
+
+
+/* 
+  Outro ponto interessante é que também podemos usar CURRENT_DATE() e NOW()
+  em conjunto com os comandos de manipulação de datas e tempo para encontrar
+  resultados dinâmicos da seguinte maneira:
+*/
+SELECT YEAR(CURRENT_DATE()); -- retorna o ano atual
+SELECT HOUR(NOW()); -- retorna a hora atual
+
+
+/*
+  Para fixar:
+    Monte uma query que exiba a diferença de dias entre '2030-01-20' e hoje.
+    Monte uma query exiba a diferença de horas entre '10:25:45' e '11:00:00'.
+*/
